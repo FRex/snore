ceba931e36e423a020cc7fe58bc296aa91e0dd9d
FRex <blazej.roszkowski@gmail.com>
2026-09-11 00:06:18 +0200

    add -s option to not add build info to the binary

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ CALVER=$(date '+v%Y_%m_%d')
 CALNUM=$(date '+%Y%m%d')
 
 WINEXE="snore32${CALVER}.exe"
-PDBFILE="snore32${CALVER}.pdb"
+# PDBFILE="snore32${CALVER}.pdb"
 
 LINEXE="snore32${CALVER}.linux"
 
 echo "Building $WINEXE and $LINEXE"
-zig cc -o "$WINEXE" snore.c -O3 -DSNORE_VERSION="$CALNUM" -target x86-windows-gnu &
-zig cc -o "$LINEXE" snore.c -O3 -DSNORE_VERSION="$CALNUM" -target x86-linux-musl &
+zig cc -s -o "$WINEXE" snore.c -O3 -DSNORE_VERSION="$CALNUM" -target x86-windows-gnu &
+zig cc -s -o "$LINEXE" snore.c -O3 -DSNORE_VERSION="$CALNUM" -target x86-linux-musl &
 wait
 # strip "$LINEXE"
-rm "$PDBFILE"
+# rm "$PDBFILE"
 
 sha1sum "$WINEXE" "$LINEXE"
 
